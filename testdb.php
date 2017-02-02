3a0fc69c954efc5e12220c3a5aa82ef15458af3f
DJ Kehoe <kehoed@njit.edu>
2017-02-01 20:31:21 -0500

    added in sanitization code to the db library

--- testdb.php
+++ testdb.php
@@ -4,9 +4,19 @@
 require_once("studentDB.inc");
 
 echo "executing script: ".$argv[0].PHP_EOL;
-
+$studentName = $argv[1];
+$studentId = $argv[2];
+$studentAddress = $argv[3];
+$studentYear = $argv[4];
 
 $studentDB = new StudentAccess("Classes");
+
+$studentDB->addStudentRecord(
+	$studentName,
+	$studentId,
+	$studentAddress,
+	$studentYear
+);
 
 $students = $studentDB->getStudentRecords();
 
