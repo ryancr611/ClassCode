43233a03fbccb9a08a5148b01ace4deb19f2c478
DJ Kehoe <kehoed@njit.edu>
2017-02-08 18:57:59 -0500

    added basic user authentication logic

--- testdb.php
+++ testdb.php
@@ -2,25 +2,54 @@
 <?php
 
 require_once("studentDB.inc");
+$action = NULL;
+for ($i = 1;$i < $argc;$i++)
+{
+	switch($argv[$i])
+	{
+		case "--auth":
+			$action = "auth";
+			break;
+		case "-u":
+			$username = $argv[$i + 1];
+			$i++;
+			break;
+		case "-p":
+			$password = $argv[$i + 1];
+			$i++;
+			break;
+	}
+}
+switch ($action)
+{
+	case "auth":
+		if (!isset($username))
+		{
+			echo "please provide a username with -u <username>".PHP_EOL;
+			exit(1);
+		}
+		if (!isset($password))
+		{
+			echo "please provide a password with -p <username>".PHP_EOL;
+			exit(1);
+		}
+		$studentDB = new StudentAccess("Classes");
+		if ($studentDB->validateUser($username,$password) == false)
+		{
+			echo "login failed!".PHP_EOL;
+		}
+		else
+		{
+			echo "login successful".PHP_EOL;
+		}
+		break;
+	default:
+		echo "No action specified, exiting".PHP_EOL;
+		exit (1);
 
-echo "executing script: ".$argv[0].PHP_EOL;
-$studentName = $argv[1];
-$studentId = $argv[2];
-$studentAddress = $argv[3];
-$studentYear = $argv[4];
+}
 
-$studentDB = new StudentAccess("Classes");
 
-$studentDB->addStudentRecord(
-	$studentName,
-	$studentId,
-	$studentAddress,
-	$studentYear
-);
 
-$students = $studentDB->getStudentRecords();
-
-echo "student records in db are:".PHP_EOL;
-print_r($students);
 echo $argv[0]." complete".PHP_EOL;
 ?>
